3144175d9e81e7502fecbad74cfdc9bd0c50986e
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-16 18:48:14 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 app.use(cors());
