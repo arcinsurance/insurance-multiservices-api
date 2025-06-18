51918c85c73260e3126c3b085586d8a1e50a6943
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-17 23:18:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from 'express';
-import importClientsRoute from './routes/importClients.js';
 import cors from 'cors';
+import dotenv from 'dotenv';
+import sendCommunicationEmail from './routes/sendCommunicationEmail.js';
+
+dotenv.config(); // Cargar variables de entorno
 
 const app = express();
+const port = process.env.PORT || 10000;
+
 app.use(cors());
 app.use(express.json());
+app.use('/api', sendCommunicationEmail); // RUTA ACTIVADA AQUÍ
 
-app.use('/api', importClientsRoute);
-
-const PORT = process.env.PORT || 10000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
