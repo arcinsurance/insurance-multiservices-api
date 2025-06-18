7eab9f74d653e0ab64bce191ce5139bcbc1d49a1
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-17 23:30:11 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -3,14 +3,16 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import sendCommunicationEmail from './routes/sendCommunicationEmail.js';
 
-dotenv.config(); // Cargar variables de entorno
+dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 10000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/api', sendCommunicationEmail); // RUTA ACTIVADA AQUÍ
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api', sendCommunicationEmail);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
