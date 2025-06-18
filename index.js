78a948834ce933df4072f1c4c24860ebb79f46e2
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-17 23:57:32 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 import express from 'express';
-import cors from 'cors';
 import dotenv from 'dotenv';
-import sendCommunicationEmail from './routes/sendCommunicationEmail.js';
+import multer from 'multer';
+import sendSignatureRequest from './routes/sendSignatureRequest.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 10000;
 
-app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api', sendCommunicationEmail);
+const upload = multer();
+app.post('/api/send-signature-request', upload.single('pdf'), sendSignatureRequest);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
