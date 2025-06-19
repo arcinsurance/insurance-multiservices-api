7de3cfad82cfa899760e67eb6cec7824465521c5
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-18 23:41:52 -0400

    Add files via upload

--- routes/templates.js
+++ routes/templates.js
@@ -4,18 +4,21 @@ const fs = require('fs');
 const path = require('path');
 const router = express.Router();
 
+// GET /api/templates/:fileName/content
 router.get('/:fileName/content', (req, res) => {
   const fileName = req.params.fileName;
-  const filePath = path.join(__dirname, '..', 'Templates', fileName);
+  const filePath = path.join(__dirname, '..', 'templates', fileName);
 
-  fs.readFile(filePath, 'utf8', (err, data) => {
+  if (!fs.existsSync(filePath)) {
+    return res.status(404).send('Plantilla no encontrada');
+  }
+
+  fs.readFile(filePath, 'utf8', (err, content) => {
     if (err) {
-      console.error(`Error al leer la plantilla ${fileName}:`, err.message);
-      return res.status(404).send('Plantilla no encontrada');
+      return res.status(500).send('Error al leer la plantilla');
     }
-
     res.set('Content-Type', 'text/html');
-    res.send(data);
+    res.send(content);
   });
 });
 
