91f51cc422667591ddc5d64502f9f16a786a77c9
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-19 15:48:55 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
@@ -6,13 +7,21 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 10000;
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'https://crm.insurancemultiservices.com',
+    'http://localhost:5173',
+    'https://insurancemultiservices.com'
+  ],
+  credentials: true
+}));
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/send-email', require('./routes/sendcommunicationemail'));
 app.use('/api/send-signature', require('./routes/sendsignaturerequest'));
-app.use('/api/templates', require('./routes/templates')); // 👈 Esta línea permite servir las plantillas
+app.use('/api/templates', require('./routes/templates'));
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
