099d1cdbc7b6d63562b59ab98c2ccab379e5a2ee
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-19 20:10:13 -0400

    Delete routes/sendsignaturerequest.js

--- routes/sendsignaturerequest.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const sendDocumentToSignature = require('../controllers/sendSignatureRequest');
-
-router.post('/', sendDocumentToSignature);
-
-module.exports = router;
