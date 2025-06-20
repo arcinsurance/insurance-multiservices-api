c513e902a6e3692b33d085b0f28676b0fa7487ae
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-19 21:44:32 -0400

    Add files via upload

--- models/Agent.js
+++ models/Agent.js
@@ -1,28 +1,10 @@
 const mongoose = require('mongoose');
 
 const agentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  role: {
-    type: String,
-    enum: ['admin', 'agent'],
-    default: 'agent',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  }
-});
+  name: String,
+  email: { type: String, unique: true },
+  password: String,
+  hasChangedInitialPassword: { type: Boolean, default: false }
+}, { timestamps: true });
 
 module.exports = mongoose.model('Agent', agentSchema);
