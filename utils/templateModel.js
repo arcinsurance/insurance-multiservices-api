a765bb527eebbc1fe2da4d2ab6c35d536696ac1d
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-27 13:34:17 -0400

    Delete utils directory

--- utils/templateModel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-
-const TemplateSchema = new mongoose.Schema({
-  fileName: { type: String, required: true },
-  displayName: { type: String, required: true },
-  uploadedAt: { type: Date, default: Date.now }
-});
-
-export default mongoose.model('Template', TemplateSchema);
