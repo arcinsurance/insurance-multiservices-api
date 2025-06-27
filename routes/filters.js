e481e350e65d3075da55ec8a1564d23a509330c0
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-27 13:32:47 -0400

    Delete routes directory

--- routes/filters.js
+++
@@ -1,8 +0,0 @@
-
-const express = require('express');
-const router = express.Router();
-const filtersController = require('../controllers/filtersController');
-
-router.get('/', filtersController.getAllFilters);
-
-module.exports = router;
