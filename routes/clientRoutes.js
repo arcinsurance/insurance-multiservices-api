a8c90b0d24ab34b9d4cccf338f1360669653b374
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-28 00:01:18 -0400

    Add files via upload

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const clientController = require('../controllers/clientController');
 const auth = require('../middleware/auth');
 
-// Importar clientes
 router.post('/import', auth, clientController.importClients);
 
 module.exports = router;
