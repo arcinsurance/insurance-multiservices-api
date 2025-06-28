b71ca6bc2b339dbe3bcddc8d25c5b31bb067294d
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-28 00:00:37 -0400

    Add files via upload

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,4 +1,3 @@
-// Middleware para verificar usuario y rol de admin
 module.exports = (req, res, next) => {
   if (!req.user || req.user.role !== 'admin') {
     return res.status(403).json({ message: 'Acceso restringido' });
