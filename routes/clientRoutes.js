2bc8853503010f82c1c6ed177f8f9bfe068343f0
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-06-28 03:22:00 -0400

    Update clientRoutes.js

--- routes/clientRoutes.js
+++ routes/clientRoutes.js
@@ -5,4 +5,7 @@ const auth = require('../middleware/auth');
 
 router.post('/import', auth, clientController.importClients);
 
+// 🚀 NUEVA RUTA: Crear cliente individual
+router.post('/', auth, clientController.createClient);
+
 module.exports = router;
