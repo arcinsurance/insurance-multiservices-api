1ff273a7fd2c3a7d9cca0dffc849caf061e11cab
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-07-02 13:00:03 -0400

    Add files via upload

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,6 @@ app.use(express.json());
 app.use('/api/clients', clientRoutes);
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
