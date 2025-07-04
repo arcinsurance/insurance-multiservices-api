79ca9ae7e210a97c4dbe419f37463d1e1c0d474d
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-07-03 22:45:41 -0400

    Update productCategories.ts

--- src/routes/productCategories.ts
+++ src/routes/productCategories.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import {
   getAllProductCategories,
   createProductCategory,
+  updateProductCategory,
   deleteProductCategory,
 } from '../controllers/productCategoryController';
 
@@ -9,6 +10,7 @@ const router = express.Router();
 
 router.get('/', getAllProductCategories);
 router.post('/', createProductCategory);
+router.put('/:id', updateProductCategory);   // ← editar
 router.delete('/:id', deleteProductCategory);
 
 export default router;
