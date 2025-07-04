fc0aa1ecd3e670057877c06d7603b366cc20d5b1
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-07-03 21:22:09 -0400

    Update products.ts

--- src/routes/products.ts
+++ src/routes/products.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
-import { getAllProducts, createProduct, deleteProduct } from '../controllers/productController';
+import {
+  createProduct,
+  getAllProducts,
+  updateProduct,
+  deleteProduct
+} from '../controllers/productController';
 
 const router = Router();
 
-router.get('/', getAllProducts);
 router.post('/', createProduct);
+router.get('/', getAllProducts);
+router.put('/:id', updateProduct);
 router.delete('/:id', deleteProduct);
 
 export default router;
