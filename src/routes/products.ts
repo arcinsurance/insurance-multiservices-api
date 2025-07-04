b030d26dde32c2ad180c82c61c3a3c2279a899e7
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-07-03 21:17:20 -0400

    Create products.ts

---
+++ src/routes/products.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { getAllProducts, createProduct, deleteProduct } from '../controllers/productController';
+
+const router = Router();
+
+router.get('/', getAllProducts);
+router.post('/', createProduct);
+router.delete('/:id', deleteProduct);
+
+export default router;
