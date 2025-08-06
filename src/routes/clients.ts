d97e71872c7872aa3b4c928079b262ab360689e6
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-08-05 22:40:01 -0400

    Update clients.ts

--- src/routes/clients.ts
+++ src/routes/clients.ts
@@ -3,11 +3,7 @@ import {
   getClients,
   getClientById,
   createClient,
-  updateClient,
-  deleteClient,
-  updateClientEmployment,
-  updateClientImmigration,
-  updateClientAddresses
+  updateClient
 } from '../controllers/clientController';
 
 import { createPolicy } from '../controllers/policyController';
@@ -19,14 +15,8 @@ router.get('/', getClients);
 router.get('/:id', getClientById);
 router.post('/', createClient);
 router.put('/:id', updateClient);
-router.delete('/:id', deleteClient);
 
-// 🆕 Detalles adicionales del cliente
-router.put('/:id/employment', updateClientEmployment);
-router.put('/:id/immigration', updateClientImmigration);
-router.put('/:id/addresses', updateClientAddresses);
-
-// 🆕 Agregar póliza
+// Agregar póliza a un cliente
 router.post('/:clientId/policies', createPolicy);
 
 export default router;
