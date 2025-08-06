f9134c6f594b0f21133b3515ed2913623e59e262
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-08-05 21:38:23 -0400

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,5 +1,14 @@
 // src/models/index.ts
 
-import * as db from '../models';
-// Si agregas más modelos, agrégalos aquí.
-// Ejemplo: export * from './incomeSource';
+export * from './client';
+
+// Si ya tienes estos archivos de modelos, descomenta y agrega aquí.
+// export * from './incomeSource';
+// export * from './address';
+// export * from './immigrationDetails';
+// export * from './employment';
+// export * from './policy';
+// export * from './carrier';
+// export * from './user';
+
+// Agrega aquí los demás modelos según los vayas creando.
