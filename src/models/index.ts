7774050d843bd39a327b4f78db9bab8f287bab53
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-08-05 22:18:34 -0400

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,12 +1,55 @@
-// src/models/index.ts
+import Client from './client';
+import Address from './address';
+import IncomeSource from './incomeSource';
+import ImmigrationDetails from './immigrationDetails';
+import User from './user';
+import Policy from './policy';
+import Lead from './lead';
+import Product from './product';
+import ProductCategory from './productCategory';
+import Carrier from './carrier';
+import DocumentTemplate from './documentTemplate';
+import SignedDocument from './signedDocument';
+import Message from './message';
+import ChatMessage from './chatMessage';
+import OtpCode from './otpCode';
+import CommissionRate from './commissionRate';
+import AgencyProfile from './agency_profile';
+import Agent from './agent';
+import AgentLicense from './agent_license';
+import AgentPermission from './agent_permission';
+import AppSettings from './app_settings';
+import Settings from './settings';
+import SettingsLog from './settings_log';
+import Category from './category';
+import ClientNote from './client_note';
 
-export * from './client';
-export * from './incomeSource';
-export * from './address';
-export * from './immigrationDetails';
-// export * from './employment';
-// export * from './policy';
-// export * from './carrier';
-// export * from './user';
+// Si no usas algunos de estos modelos, elimina el import y el export correspondiente.
 
-// Agrega aquí los demás modelos según los vayas creando.
+export {
+  Client,
+  Address,
+  IncomeSource,
+  ImmigrationDetails,
+  User,
+  Policy,
+  Lead,
+  Product,
+  ProductCategory,
+  Carrier,
+  DocumentTemplate,
+  SignedDocument,
+  Message,
+  ChatMessage,
+  OtpCode,
+  CommissionRate,
+  AgencyProfile,
+  Agent,
+  AgentLicense,
+  AgentPermission,
+  AppSettings,
+  Settings,
+  SettingsLog,
+  Category,
+  ClientNote,
+};
