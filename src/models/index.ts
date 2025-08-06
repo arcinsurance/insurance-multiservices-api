fee30957b204191a5a41bd2e583f5503e5538493
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-08-05 21:53:25 -0400

    Update index.ts

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,12 +3,12 @@
 export * from './client';
 
 // Si ya tienes estos archivos de modelos, descomenta y agrega aquí.
-// export * from './incomeSource';
-// export * from './address';
-// export * from './immigrationDetails';
-// export * from './employment';
-// export * from './policy';
-// export * from './carrier';
-// export * from './user';
+export * from './incomeSource';
+export * from './address';
+export * from './immigrationDetails';
+export * from './employment';
+export * from './policy';
+export * from './carrier';
+export * from './user';
 
 // Agrega aquí los demás modelos según los vayas creando.
