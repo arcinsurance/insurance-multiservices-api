35eb90ebbe8ea09a98f30f48e73cb8b9623edc83
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-08-07 20:15:28 -0400

    Delete src/src directory

--- src/src/routes/leadRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getLeads, createLead } from '../controllers/leadController';
-
-const router = Router();
-
-router.get('/', getLeads);
-router.post('/', createLead);
-
-export default router;
--- src/src/services/userService.ts
+++
@@ -1,7 +0,0 @@
-// src/services/userService.ts
-import axios from 'axios';
-
-export const createAgent = async (agentData: any) => {
-  const response = await axios.post('/api/agents', agentData);
-  return response.data;
-};
