f28438f62ec73ab301a6d7f9f62a9aeea1a9869f
arcinsurance <arc.insurancemultiservices@gmail.com>
2025-08-12 13:43:39 -0400

    Create validate.ts

---
+++ src/middlewares/validate.ts
@@ -0,0 +1,17 @@
+import { AnyZodObject } from 'zod';
+import { Request, Response, NextFunction } from 'express';
+
+export const validate =
+  (schema: AnyZodObject) =>
+  (req: Request, res: Response, next: NextFunction) => {
+    const result = schema.safeParse(req.body);
+    if (!result.success) {
+      return res.status(400).json({
+        message: 'Validation error',
+        errors: result.error.flatten(),
+      });
+    }
+    // Reemplaza body normalizado (aplica defaults)
+    req.body = result.data;
+    next();
+  };
